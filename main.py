e92ed3a39ef989e084a76f68766e5234a121d7be
Travis CI <travis@travis-ci.org>
2024-12-18 18:35:58 +0300

    fff

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ def monitor_programs():
             send_telegram_message(f"Ошибка мониторинга: {e}")
 
 
-if __name__ == "__main__":
-    import uvicorn
+# if __name__ == "__main__":
+#     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+#     uvicorn.run(app, host="0.0.0.0", port=5000)
